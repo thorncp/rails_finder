f950c259cb12ba585e4bc49b89f17cc327e3f78b
Chris Thorn <thorncp@gmail.com>
2013-01-28 22:33:04 -0900

    Cleanup spec a bit

--- spec/rails_finder/gemfile_spec.rb
+++ spec/rails_finder/gemfile_spec.rb
@@ -11,48 +11,58 @@ END
 
 module RailsFinder
   describe Gemfile do
-    it "knows if file exists" do
-      Dir.mktmpdir do |dir|
-        FileUtils.touch(File.join(dir, "Gemfile"))
+    def with_dir
+      Dir.mktmpdir("rails_version-spec") do |dir|
+        yield dir
+      end
+    end
 
-        Gemfile.new(File.join(dir, "Gemfile")).should exist
+    def gemfile(dir)
+      File.join(dir, "Gemfile")
+    end
+
+    it "knows if file exists" do
+      with_dir do |dir|
+        FileUtils.touch(gemfile(dir))
+
+        Gemfile.new(gemfile(dir)).should exist
       end
     end
 
     it "knows if file does not exist" do
-      Dir.mktmpdir do |dir|
-        Gemfile.new(File.join(dir, "Gemfile")).should_not exist
+      with_dir do |dir|
+        Gemfile.new(gemfile(dir)).should_not exist
       end
     end
 
     it "reports Rails version" do
-      Dir.mktmpdir do |dir|
-        File.open(File.join(dir, "Gemfile"), "w") do |file|
+      with_dir do |dir|
+        File.open(gemfile(dir), "w") do |file|
           file.puts(VALID_GEMFILE)
         end
 
-        Gemfile.new(File.join(dir, "Gemfile")).rails_version.should == "3.2.11"
+        Gemfile.new(gemfile(dir)).rails_version.should == "3.2.11"
       end
     end
 
     it "reports none when no Rails version" do
-      Dir.mktmpdir do |dir|
-        File.open(File.join(dir, "Gemfile"), "w") do |file|
+      with_dir do |dir|
+        File.open(gemfile(dir), "w") do |file|
           file.puts "source :rubygems"
           file.puts "gem 'rspec', '2.12.0'"
         end
 
-        Gemfile.new(File.join(dir, "Gemfile")).rails_version.should == "n/a"
+        Gemfile.new(gemfile(dir)).rails_version.should == "n/a"
       end
     end
 
     it "memoizes rails version" do
-      Dir.mktmpdir do |dir|
-        File.open(File.join(dir, "Gemfile"), "w") do |file|
+      with_dir do |dir|
+        File.open(gemfile(dir), "w") do |file|
           file.puts(VALID_GEMFILE)
         end
 
-        @subject = Gemfile.new(File.join(dir, "Gemfile"))
+        @subject = Gemfile.new(gemfile(dir))
         @subject.rails_version.should == "3.2.11"
       end
 
